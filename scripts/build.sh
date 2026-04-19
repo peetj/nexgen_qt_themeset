af2c4b6c6b291d8a89c2b9ab90876439b0d8e004
Peter Januarius <pete@nexgencodecamp.com.au>
2026-04-19 15:32:47 +1000

    Add scripts to build/run with Git Bash

---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+QT_PREFIX="${QT_PREFIX:-/c/Qt/6.11.0/mingw_64}"
+BUILD_DIR="${BUILD_DIR:-build}"
+GENERATOR="${GENERATOR:-Ninja}"
+
+cmake -S . -B "$BUILD_DIR" -G "$GENERATOR" -DCMAKE_PREFIX_PATH="$QT_PREFIX"
+cmake --build "$BUILD_DIR"
